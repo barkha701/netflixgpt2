b9e9e18ff1d89d7cb70cc10c05546a9a948b2c08
barkha701 <barkhaag701@gmail.com>
2025-07-29 23:10:11 +0530

    basics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Body from './components/Body';
 import "tailwindcss";
+import { Provider } from 'react-redux';
+import appStore from './utils/appStore';
 
 function App() {
 
   return (
 
     <div>
+      <Provider store = {appStore}>
     <Body/>
+    </Provider>
     </div>
 
   );
--- src/components/Body.js
+++ src/components/Body.js
@@ -2,8 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Browse from "./Browse";
 import Login from "./Login";
 import { RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
 
 const Body = () => {
+    const dispatch = useDispatch();
     const appRouter = createBrowserRouter([
         {
             path: "/" ,
@@ -15,7 +19,21 @@ const Body = () => {
             element: <Browse/>
         }
     ])
-    return (
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+              // https://firebase.google.com/docs/reference/js/auth.user
+              const uid = user.uid;
+              dispatch(addUser(uid));
+             // const {uid,email, displayname} = user;
+            } else {
+              dispatch(removeUser());
+            }
+          });
+
+    }, []);
+
+return (
 <div>
     <RouterProvider router = {appRouter} />
 </div> );
